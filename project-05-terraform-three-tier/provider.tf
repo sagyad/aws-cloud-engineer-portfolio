dd4df17960780a3274118781d0c2f4b84f7fba12
Sagar Yadav <svyadav@gmail.com>
2026-09-21 15:39:10 +0100

    Switch to S3 native lockfile, remove deprecated dynamodb_table

--- project-05-terraform-three-tier/provider.tf
+++ project-05-terraform-three-tier/provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "sy-terraform-state-bucket-eu-west-2"
-    key            = "project-05/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "ddb-sy-terraform-lock"
-    encrypt        = true
+    bucket       = "sy-terraform-state-bucket-eu-west-2"
+    key          = "project-05/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 
 }
