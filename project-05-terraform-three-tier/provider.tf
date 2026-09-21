4f072564ecf28970a414a79dc72d172315ed70d4
Sagar Yadav <svyadav@gmail.com>
2026-09-21 15:32:20 +0100

    Add S3 remote backend for Terraform state management

--- project-05-terraform-three-tier/provider.tf
+++ project-05-terraform-three-tier/provider.tf
@@ -5,7 +5,16 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket         = "sy-terraform-state-bucket-eu-west-2"
+    key            = "project-05/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "ddb-sy-terraform-lock"
+    encrypt        = true
+  }
+
 }
+
 
 provider "aws" {
   region = "eu-west-2"
